f5cd721cd3f1c860125e4be8b83aed5f98b2c927
JazPin <jpin1@studentmail.nait.ca>
2014-10-15 11:12:04 -0600

    Add ASP.NET website and solution

---
+++ DVCS_Excercise/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Excercise.Startup))]
+namespace DVCS_Excercise
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
